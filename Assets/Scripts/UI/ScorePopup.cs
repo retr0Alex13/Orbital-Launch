5a229ab56e98b2c6b14aa2f662fd26868c6c1d17
Oleksandr Moskalyk <83085784+retr0Alex13@users.noreply.github.com>
2026-06-27 20:16:44 +0300

    Create score popup animation

--- Assets/Scripts/UI/ScorePopup.cs
+++ Assets/Scripts/UI/ScorePopup.cs
@@ -1,6 +1,6 @@
 using TMPro;
 using UnityEngine;
-
+using PrimeTween;
 
 public class ScorePopup : MonoBehaviour
 {
@@ -14,9 +14,32 @@ public class ScorePopup : MonoBehaviour
     [SerializeField]
     private OrbitEntryConfig orbitEntryConfig;
 
+    [SerializeField]
+    private RectTransform popupTransform;
+
+    [SerializeField]
+    private CanvasGroup canvasGroup;
+
+    private Vector2 startPosition;
+
+    private void Awake()
+    {
+        startPosition = popupTransform.anchoredPosition;
+    }
+
     public void SetScore(OrbitEntryType entryType)
     {
+        popupTransform.anchoredPosition = startPosition;
+        canvasGroup.alpha = 1;
+
         skillText.text = ScoreManager.Instance.GetLabel(entryType);
         scoreText.text = $"+{ScoreManager.Instance.GetPoints(entryType, orbitEntryConfig)}";
+        AnimateAndHide();
+    }
+
+    private void AnimateAndHide()
+    {
+        Tween.UIAnchoredPositionY(popupTransform, endValue: startPosition.y + 100f, duration: 1f, ease: Ease.InOutSine);
+        Tween.Alpha(canvasGroup, startValue: 1f, endValue: 0, duration: 1f, ease: Ease.InOutSine);
     }
 }
